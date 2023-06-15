e8a2df38d13ec2c11da1b551a6233b8714487992
olehae <das.typ.2003@gmail.com>
2023-06-15 14:16:02 +0200

    Changed Plots

--- mergesort.py
+++ mergesort.py
@@ -43,9 +43,13 @@ import matplotlib.pyplot as plt
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
-plt.plot(x, my_list)
+plt.bar(x, my_list)
+plt.xticks([])
+plt.title("Unsorted Values")
 plt.show()
 mergeSort(my_list)
 x = range(len(my_list))
-plt.plot(x, my_list)
+plt.bar(x, my_list)
+plt.xticks([])
+plt.title("Sorted Values")
 plt.show()
